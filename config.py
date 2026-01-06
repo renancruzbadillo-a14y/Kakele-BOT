964fbb214781e812264b7dbf0bd7265c47b4699b
renancruzbadillo-a14y <renan.cruz.badillo@gmail.com>
2026-01-06 14:00:39 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# config.py — configurações do bot
+
+TOKEN = "MTQ1ODEwODA4NTk0NzA3Njc2MA.G_2AoW.X_lIoTEIHQkk54dW0Wp9g74B7_gpiY9YuxL6uM"
+SERVER = "south-america-pink"
+
+# ID do servidor Discord
+GUILD_ID = 1458092203204411559
+
+# ID do canal onde o bot vai enviar mensagens/eventos
+EVENT_CHANNEL_ID = 0 # troque por um ID real se quiser eventos
